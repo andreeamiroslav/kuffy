b772486c4926eb90e9110332e4355a25b3179bcf
Federico Cattini <b994@live.com>
2019-05-19 17:43:42 +0200

    xd

---
+++ testone.php
@@ -0,0 +1,25 @@
+<?php
+require_once("config.php");
+$link = mysqli_connect(DBMS_HOST, DBMS_USER, DBMS_PASSWORD, DBMS_DB );
+
+if (!$link) {
+  exit;
+}
+
+$query = "SELECT * FROM utenti";
+
+$rs = mysqli_query($link, $query);
+
+if(!$rs == mysqli_query($link, $query)){
+  echo "errore";
+  exit;
+}
+
+$num_row = mysqli_num_rows($rs);
+
+if($num_row==1){
+  echo "ciao";
+}
+mysqli_close($link);
+
+ ?>
