a602616c489e8af82a3c849fa2efc6c3d51ca5ed
Federico Cattini <b994@live.com>
2019-05-19 18:57:21 +0200

    - Ora per davvero

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
     require_once("config.php");
 
     if(!$_SESSION['isLogged']){
-      header('Location: index.html');
+      header('Location: index.php');
     }
 
     $link = mysqli_connect(DBMS_HOST, DBMS_USER, DBMS_PASSWORD, DBMS_DB );
