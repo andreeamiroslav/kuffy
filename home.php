3f4fe5117ad20bc2b3659e6716d4e51265aec3b8
MeatballInSauce <50716646+MeatballInSauce@users.noreply.github.com>
2019-05-19 22:55:42 +0200

    - Corrected icons' path

--- home.php
+++ home.php
@@ -146,9 +146,9 @@
     <div class="container">
       <h6 class="white-text" style = "position:relative; left:auto;; top:-20px;">Creato da ______ - <a class="grey-text text-lighten-3" href="#!">Versione Mobile</a></h6>
       <h7 class="grey-text text-lighten-4" style = "position:relative; left:auto;; top:-15px;">Seguici su:</h7>
-      <br /><a href="#!"><img src="Icone/internet.ico" style = "position:relative; left:auto;; top:-12px;" width="28" height="28" title="Facebook" alt="Facebook"></a>
-      <a href="#!"><img src="Icone/Twitter-icon.png" style = "position:relative; left:auto;; top:-10px;" width="30" height="30" title="Twitter" alt="Twitter"></a>
-      <a href="#!"><img src="Icone/1491580658-yumminkysocialmedia06_83104.png" style = "position:relative; left:auto;; top:-12px;" width="27" height="27" title="Instagram" alt="Instagram"></a>
+      <br /><a href="#!"><img src="/icone/internet.ico" style = "position:relative; left:auto;; top:-12px;" width="28" height="28" title="Facebook" alt="Facebook"></a>
+      <a href="#!"><img src="/icone/Twitter-icon.png" style = "position:relative; left:auto;; top:-10px;" width="30" height="30" title="Twitter" alt="Twitter"></a>
+      <a href="#!"><img src="/icone/1491580658-yumminkysocialmedia06_83104.png" style = "position:relative; left:auto;; top:-12px;" width="27" height="27" title="Instagram" alt="Instagram"></a>
     </div>
   </footer>
   </body>
