bb8846fd9f3f79c6413241338414798ea4b78503
Federico Cattini <b994@live.com>
2019-05-19 18:10:25 +0200

    a

--- testone.php
+++ testone.php
@@ -1,11 +1,3 @@
 <?php
-echo "ciao";
-require_once("config.php");
-echo "lol";
-echo var_dump(function_exists('mysqli_connect'));
-echo "lol";
-if ($link->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
+phpinfo();
  ?>
