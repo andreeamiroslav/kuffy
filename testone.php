760df02b4c805c10e793c49e313ae125ac56da97
Federico Cattini <b994@live.com>
2019-05-19 18:06:22 +0200

    s

--- testone.php
+++ testone.php
@@ -3,7 +3,7 @@ echo "ciao";
 require_once("config.php");
 echo "lol";
 $link = mysqli_connect('127.0.0.1', 'utentetest', 'passwordtest', 'test');
-
+echo "lol";
 if ($link->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
