0c714e2b2a92c6033f2f4b0ca581ed496cc91e35
Federico Cattini <b994@live.com>
2019-05-19 17:58:20 +0200

    a

--- testone.php
+++ testone.php
@@ -2,7 +2,9 @@
 echo "ciao";
 require_once("config.php");
 echo "lol";
-$link = mysqli_connect(DBMS_HOST, DBMS_USER, DBMS_PASSWORD, DBMS_DB );
-echo "ciao";
 
+if (mysqli_connect(DBMS_HOST, DBMS_USER, DBMS_PASSWORD, DBMS_DB)->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
  ?>
