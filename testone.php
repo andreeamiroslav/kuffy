b455ec3a4921c8f3aedc37f097f6b96a5b619586
Federico Cattini <b994@live.com>
2019-05-19 18:22:47 +0200

    a

--- testone.php
+++ testone.php
@@ -1,3 +1,27 @@
 <?php
-phpinfo();
+require_once("config.php");
+$link = mysqli_connect(DBMS_HOST, DBMS_USER, DBMS_PASSWORD, DBMS_DB );
+
+if (!$link) {
+  exit;
+}
+
+$query = "SELECT * FROM utenti WHERE utente_email='".$email."' AND utente_password='".$password."';";
+
+$rs = mysqli_query($link, $query);
+
+if(!$rs == mysqli_query($link, $query)){
+  echo "errore";
+  exit;
+}
+
+$num_row = mysqli_num_rows($rs);
+
+if($num_row==1){
+  $ret = TRUE;
+  $row = mysqli_fetch_assoc($rs);
+  $_SESSION['utente_id'] = $row['utente_id'];
+}
+mysqli_close($link);
+
  ?>
