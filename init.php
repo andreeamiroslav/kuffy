f47da30bc974bc55c9e7dd6d7c38d98bd671f32c
Federico Cattini <b994@live.com>
2019-05-19 18:56:20 +0200

    - PHP PER TUTTI

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
     require_once("config.php");
 
     if(!$_SESSION['isLogged']){
-      header('Location: login.php?login=false');
+      header('Location: index.html');
     }
 
     $link = mysqli_connect(DBMS_HOST, DBMS_USER, DBMS_PASSWORD, DBMS_DB );
