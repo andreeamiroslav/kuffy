14e38072450c13e810a8f0aa4a6841c6be7a8bac
hrmntmrs <50423661+hrmntmrs@users.noreply.github.com>
2019-05-24 23:20:54 +0200

    - added menu

--- stanze.php
+++ stanze.php
@@ -2,16 +2,18 @@
   <head>
     <title>Stanze - Kuff</title>
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="materialize.min.css"  media="screen,projection"/>
+    <script type = "text/javascript" src = "Scripts/jquery-2.1.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="materialize/css/materialize.min.css"/>
+    <script type = "text/javascript" src = "materialize/js/materialize.js"></script>
+    <script type = "text/javascript" src = "materialize/js/materialize.min.js"></script>
     <link type="text/css" rel="stylesheet" href="fogliodistile.css"/>
-    <script type="text/javascript" src="materialize.min.js"></script>
-    <script type="text/javascript" src="script.js"></script>
     <script type="text/javascript" src="Scripts/calStanzeScript.js"></script>
     <script language="javascript" type="text/javascript" src="Scripts/modal.js"></script>
   </head>
   <body onload="getStanze(<?php echo $_GET['strutturaid'] ?>);">
     <script language="javascript" type="text/javascript" src="Scripts/header.js"></script>
     <script language="javascript" type="text/javascript" src="Scripts/menu.js"></script>
+    <script>getStructures();</script>
 
     <div id="modal1" class="modal modal-footer">
         <div class="modal-content">
