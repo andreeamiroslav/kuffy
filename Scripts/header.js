9f785a363a2fb45fa439602b8c165422a63654c1
hrmntmrs <50423661+hrmntmrs@users.noreply.github.com>
2019-05-26 14:31:42 +0200

    Update header.js

--- Scripts/header.js
+++ Scripts/header.js
@@ -1,4 +1,4 @@
-document.write('<div id="page-container">  <nav><div class="nav-wrapper"><a href="#" class="brand-logo">Logo</a></div></nav><div id="content-wrap">')
+document.write('<div id="page-container">  <nav><div class="nav-wrapper"><a href="#" class="brand-logo">Kuffy</a></div></nav><div id="content-wrap">')
 
 /*Scritto decentemente:
 <div id="page-container">
