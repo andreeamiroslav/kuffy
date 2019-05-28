cf830b58fba7074bcfcdf2559d3b4adde1731b9b
hrmntmrs <50423661+hrmntmrs@users.noreply.github.com>
2019-05-28 20:04:08 +0200

    - Il secchietto della monnezza te manna su upDelStructures.php

--- Scripts/calStanzeScript.js
+++ Scripts/calStanzeScript.js
@@ -246,7 +246,7 @@ function getStrutture(){
       do{
         i++;
         doc = document.getElementById('square-edit'+i);
-        var content = '<h7>' + v[i]['struttura_nome'] +'</h7><br /><br /><img src="Icone/97805.png" id="structure-icon"><br /><br /><a href="#modal2" class="modal-trigger"><img src="Icone/61456.png" id="structure-edit"></a><a href="#?"><img src="Icone/1214428.png" id="structure-edit"></a>';
+        var content = '<h7>' + v[i]['struttura_nome'] +'</h7><br /><br /><img src="Icone/97805.png" id="structure-icon"><br /><br /><a href="#modal2" class="modal-trigger"><img src="Icone/61456.png" id="structure-edit"></a><a href="upDelStructures.php?cmd=del&strutturaid='+v[i]['struttura_id']+'"><img src="Icone/1214428.png" id="structure-edit" ></a>';
         doc.style.display = "block";
         document.getElementById('square-empty'+i).style.display="none";
         var t = i-1;
