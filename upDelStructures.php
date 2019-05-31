4ebd4a9385944abcfd0a3d2ead92fe8a86f4a650
hrmntmrs <50423661+hrmntmrs@users.noreply.github.com>
2019-05-31 17:23:46 +0200

    DAJE SE PO FA TUTTO

--- upDelStructures.php
+++ upDelStructures.php
@@ -7,5 +7,12 @@
     if($rs = mysqli_query($link, $query)){
       header('Location: strutture.php');
     }
+  } else if($_REQUEST['cmd']=='upd'){
+    $query = "UPDATE strutture SET struttura_nome='".$_REQUEST['struttura_nome']."', struttura_note='".$_REQUEST['struttura_note']."'
+      WHERE struttura_id='".$_REQUEST['strutturaid']."'";
+
+      if($rs = mysqli_query($link, $query)){
+        header('Location: strutture.php');
+      }
   }
  ?>
