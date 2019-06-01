84d7ac741d4a6add76e4688d5e8a474db22243fa
hrmntmrs <50423661+hrmntmrs@users.noreply.github.com>
2019-06-01 13:41:41 +0200

    Add files via upload

---
+++ Scripts/addRoom.js
@@ -0,0 +1,26 @@
+var formsNumber;
+
+function submitValues(strutturaid){
+  formsNumber = document.forms.length;
+  var data = "";
+  var xmlhttp = new XMLHttpRequest();
+  var i = 0;
+
+  for(i; i<formsNumber; i++){
+    var nome = document.getElementById('name-room'+i).value;
+    var postiletto = document.getElementById('beds-room'+i).value;
+    var prezzoNotte = document.getElementById('price-room'+i).value;
+    var note = document.getElementById('note-room'+i).value;
+
+    data = 'name'+i+'='+nome+'&beds'+i+'='+postiletto+'&price'+i+'='+prezzoNotte+'&note'+i+'='+note;
+    if(i+1!=formsNumber){
+      data += '&';
+    }
+    var url = document.getElementById('addF'+i).action;
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.send(data);
+  }
+
+  window.location.href = 'stanze.php?strutturaid='+strutturaid;
+}
