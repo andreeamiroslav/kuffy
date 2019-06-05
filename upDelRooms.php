216afca7fb0b1b312a20999514d49053ed2b1574
hrmntmrs <50423661+hrmntmrs@users.noreply.github.com>
2019-06-05 16:10:13 +0200

    Update upDelRooms.php

--- upDelRooms.php
+++ upDelRooms.php
@@ -8,13 +8,20 @@
       header('Location: stanze.php?strutturaid='.$_REQUEST['strutturaid']);
     }
   } else if($_REQUEST['cmd']=='upd'){
-    $query = "UPDATE stanze SET stanza_nome='".$_REQUEST['stanza_nome']."', stanza_des='".$_REQUEST['stanza_note']."',
-      stanza_postiletto='".$_REQUEST['stanza_postiletto']."', stanza_prezzonotte='".$_REQUEST['stanza_prezzonotte']."'
-      WHERE stanza_id='".$_REQUEST['stanza_id']."'";
+      if($_REQUEST['val']==1){
+        $query = "UPDATE stanze SET stanza_nome='".$_REQUEST['stanza_nome']."', stanza_des='".$_REQUEST['stanza_note']."',
+          stanza_postiletto='".$_REQUEST['stanza_postiletto']."', stanza_prezzonotte='".$_REQUEST['stanza_prezzonotte']."'
+          WHERE stanza_id='".$_REQUEST['stanza_id']."'";
+      } else {
+        $query = "UPDATE stanze SET stanza_nome='".$_REQUEST['stanza_nome']."', stanza_des='".$_REQUEST['stanza_note']."',
+          stanza_postiletto='".$_REQUEST['stanza_postiletto']."', stanza_prezzonotte='".$_REQUEST['stanza_prezzonotte']."'
+          WHERE stanza_id='".$_REQUEST['stanza_id']."'";
+      }
 
-      echo $query;
-      if($rs = mysqli_query($link, $query)){
+      if($rs = mysqli_query($link, $query) && $_REQUEST['val']==1){
         header('Location: stanze.php?strutturaid='.$_REQUEST['strutturaid']);
+      } else {
+        header('Location: stanza.php?stanzaid='.$_REQUEST['stanza_id']);
       }
   }
  ?>
