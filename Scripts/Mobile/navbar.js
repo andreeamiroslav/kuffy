3191f567a1960a033e8fcebfe18b84e324644620
Federico Cattini <b994@live.com>
2019-06-15 16:11:17 +0200

    test

--- Scripts/Mobile/navbar.js
+++ Scripts/Mobile/navbar.js
@@ -1,4 +1,30 @@
-document.write('<nav id="mobileNav"><div class="con"> <a href="#" data-target="slide-out" class="show-on-large sidenav-trigger"><img class="menuIcon" src="/Icone/Mobile/hamburgerMenu.png" /></a> <a href="index.php" id="navName">KUFFY</a></div></nav>  <ul id="slide-out" class="sidenav"><li>Ciaone</li><li>Ciaino</li>  </ul> ');
+document.write('<ul id="dropdown2" class="dropdown-content"></ul><nav id="mobileNav"><div class="con"> <a href="#" data-target="slide-out" class="show-on-large sidenav-trigger"><img class="menuIcon" src="/Icone/Mobile/hamburgerMenu.png" /></a> <a href="index.php" id="navName">KUFFY</a></div></nav><ul id="slide-out" class="sidenav"><li><a href="strutture.php" class = "dropdown-trigger" data-target="dropdown2">Strutture</a></li><li><a href="aggiungiPrenotazione.php">Aggiungi prenotazione</a></li></ul><ul id="nav-mobile" class = "right hide-on-med-and-down"><li ><a href="" id="utente" class = "dropdown-trigger" data-target="dropdown1" >Utente     <img src="Icone/user.png" id="user-icon"/></a></li></ul> ');
+
+function getStructures(){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var v = JSON.parse(this.responseText);
+      var html="";
+      var i = 0;
+      var temp = document.getElementById('dropdown2');
+      while(i != Object.keys(v).length){
+        i++;
+        html += '<li><a href="stanze.php?strutturaid=' + v[i]['struttura_id'] +'">' + v[i]['struttura_nome'] +'</a></li>';
+      }
+      document.getElementById('utente').innerHTML = v[1]['nome_utente'] + '     <img src="/Icone/user.png" id="user-icon"/></a></li>';
+      temp.innerHTML = html;
+    }
+  };
+  xmlhttp.open("GET", "Queries/queryStructures.php", true);
+  xmlhttp.send();
+  return true;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.dropdown-trigger');
+   var instances = M.Dropdown.init(elems, {hover: true});
+});
 /*
 <nav> <div class="nav-wrapper" id="menu">
   <ul id="nav-mobile" class="left hide-on-med-and-down">
