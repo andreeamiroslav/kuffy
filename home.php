608982ac73cafd69f51310267adcb49ee996d747
Federico Cattini <b994@live.com>
2019-06-16 13:15:05 +0200

    - Fix

--- home.php
+++ home.php
@@ -31,10 +31,9 @@
         <div class="input-field col s12" id="list">
           <select id="selectStructure">
             <?php require('init.php');
-            $query = "SELECT s.stanza_nome, p.id, p.from_day, p.to_day, p.nome, p.data, str.struttura_nome
+            $query = "SELECT DISTINCT str.struttura_nome
                       FROM prenotazioni p, stanze s, strutture str
-                      WHERE str.struttura_fkutenteid = '".$_SESSION['utente_id']."' AND p.id_stanza = s.stanza_id AND s.stanza_fkstrutturaid=str.struttura_id
-                      ORDER BY p.data DESC";
+                      WHERE str.struttura_fkutenteid = '".$_SESSION['utente_id']."' AND p.id_stanza = s.stanza_id AND s.stanza_fkstrutturaid=str.struttura_id";
             $result = $link->query($query);
             $i = 0;
             while($row = mysqli_fetch_array($result)){
