d6b57bb355e94fd11ad03d59686b034a09eb36c6
MeatballInSauce <50716646+MeatballInSauce@users.noreply.github.com>
2019-06-26 15:36:10 +0200

    Update header.js

--- Scripts/header.js
+++ Scripts/header.js
@@ -1,4 +1,4 @@
-document.write('<div id="page-container">  <nav><div class="nav-wrapper"><a href="#" class="brand-logo"><img src="Icone/Logo Kuffy.png" id="logo"></a></div></nav><div id="content-wrap">')
+document.write('<div id="page-container">  <nav><div class="nav-wrapper"><a href="home.php" class="brand-logo"><img src="Icone/Logo Kuffy.png" id="logo"></a></div></nav><div id="content-wrap">')
 
 /*Scritto decentemente:
 <div id="page-container">
