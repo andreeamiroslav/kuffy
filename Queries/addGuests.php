e6a08fa6206b67c67d766cb25cd58d1e9ea833b8
Federico Cattini <b994@live.com>
2019-06-28 10:57:25 +0200

    gegs

--- Queries/addGuests.php
+++ Queries/addGuests.php
@@ -1,23 +1,21 @@
 <?php
   require_once('init.php');
   $num = $_REQUEST['num'];
+
   $q = 'SELECT id FROM prenotazioni ORDER BY id DESC LIMIT 1';
   $rs= mysqli_query($link, $q);
   $row = mysqli_fetch_assoc($rs);
   $last_id = $row['id'];
 
  for($i=0; $i<=$num; $i++){
-  // if($_REQUEST['provenienza'.$i] != "" && $_REQUEST['gender'.$i] != "" && $_REQUEST['nascita'.$i] != "0000-00-00" && $_REQUEST['professione'.$i] != "" && $_REQUEST['name'.$i] != "" && $_REQUEST['surname'.$i] != ""){
-     $query = 'INSERT INTO ospiti '.'(o_prenotazioneid, o_provenienza, o_sesso, o_nascita, o_professione, o_nome, o_cognome) VALUES ("'.
-         $last_id.'", "'.
-         $_REQUEST['provenienza'.$i].'", "'.
-         $_REQUEST['gender'.$i].'", "'.
-         $_REQUEST['nascita'.$i].'", "'.
-         $_REQUEST['professione'.$i].'", "'.
-         $_REQUEST['name'.$i].'", "'.
-         $_REQUEST['surname'.$i].'");';
-         $rs = mysqli_query($link, $query);
-       }
-    // }
-   }
+   $query = 'INSERT INTO ospiti '.'(o_prenotazioneid, o_provenienza, o_sesso, o_nascita, o_professione, o_nome, o_cognome) VALUES ("'.
+       $last_id.'", "'.
+       $_REQUEST['provenienza'.$i].'", "'.
+       $_REQUEST['gender'.$i].'", "'.
+       $_REQUEST['nascita'.$i].'", "'.
+       $_REQUEST['professione'.$i].'", "'.
+       $_REQUEST['name'.$i].'", "'.
+       $_REQUEST['surname'.$i].'");';
+       $rs = mysqli_query($link, $query);
+     }
  ?>
