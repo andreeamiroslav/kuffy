c41b838a06d5d7c1e11245b73792e28c5a62e1c1
Federico Cattini <b994@live.com>
2019-07-03 20:52:15 +0200

    - Cleanup

--- Mobile/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('TITOLO', 'Gestione strutture');
-    define('DBMS_HOST', '127.0.0.1');
-    define('DBMS_USER', 'utentetest');
-    define('DBMS_PASSWORD', 'passwordtest');
-    define('DBMS_DB', 'test');
- ?>
